eb7d47514cef219542fb06e8fcabae4de27523fc
ShadowOverLord <theshadowoverlord101@gmail.com>
2022-03-09 09:34:22 +0000

    Wed09Mar22

--- Scala/UniversityWork/week23/src/main/scala/Counter1.scala
+++ Scala/UniversityWork/week23/src/main/scala/Counter1.scala
@@ -9,6 +9,13 @@ object Counter{
     def inc(n:Int): Unit={
         for(i<-1 to n) inc()
     }
+
+    def setValue(newValue:Int): Unit={
+        counter=newValue
+    }
+    def reset(): Unit={
+        counter=0
+    }
 }
 
 object UseCounter{
@@ -19,6 +26,10 @@ object UseCounter{
         Counter.inc()
         println(Counter.getValue)
         Counter.inc()
+        println(Counter.getValue)
+        Counter.setValue(10)
+        println(Counter.getValue)
+        Counter.reset()
         println(Counter.getValue)
     }
 }
