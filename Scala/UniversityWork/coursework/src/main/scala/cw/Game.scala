e36637ef900facbc7e573a9f44435f825c1d6806
Dilan Goodwin <theshadowoverlord101@gmail.com>
2022-03-30 11:35:18 +0100

    Wed30Mar22

--- Scala/UniversityWork/coursework/src/main/scala/cw/Game.scala
+++ Scala/UniversityWork/coursework/src/main/scala/cw/Game.scala
@@ -63,140 +63,52 @@ class Game(wall: List[(Int, Int)], coin: List[(Int, Int, Int)], initialX: Int, i
   def moveUp(n: Int) {
   }
 
-  /**
-   * Move the player n places down. Negative numbers or 0 as a parameter cause no effect.
-   * If there is a wall or the field ends, the player stops before the wall or end of the field.
-   * Any coins are collected (i.e. a call to checkCoin() is made after each move).
-   * A more advanced requirement would be to call checkCoins() if completed.
-   */
   def moveDown(n: Int) {
   }
 
-  /**
-   * Checks if the current position is a coin. A coin exists if the cell
-   * has a value larger than 0. If a coin does exist, increase the score,
-   * and then erase the coin, i.e. set it to -1.
-   */
   def checkCoin() {
   }
 
-  //The methods beyond this point (aside to those in GameBuilder which is a separate task) are more complex than those above.
-
-  /**
-   * This moves the player according to a string. The string can contain the
-   * letters w, a, s, d representing up, left, down, right moves. If
-   * there is a wall or the field ends, the individual move is not
-   * executed. Any further moves are done. Any coins are collected and the
-   * save position is evaluated.
-   */
   def move(s: String) {
   }
 
-  /**
-   * Identifies the maximum overall score in the game. This is the sum
-   * of the current score and the possible score from collecting all of the remaining coins.
-   * No coins are collected here, only the max score is returned.
-   */
   def maxScore(): Int = 0
 
-  /**
-   * Checks if the rectangle defined by the current position and saved position
-   * covers nine or more positions. If yes, it collects coins in it, increases the
-   * score, and erases the coins. Also resets the saved position to -1,-1.
-   */
   def checkCoins() {
   }
 
-  /**
-   * This gives a string in the format for move, which collects all the available coins. No specific
-   * requirements for the efficiency of the solution exist, but the solution must consist of a finite number
-   * of steps. The move is combined of a number of moves given by suggestMove.
-   * If these are not possible, an empty string is returned. No coins are collected
-   * and the player must be at the original position after the execution of the method.
-   */
   def suggestSolution(): String = ""
 
-  /**
-   * This gives a string in the format for move, which moves from the current position to
-   * position x,y. No specific requirements for the efficiency of the solution exist. The move
-   * cannot jump walls. The method is restricted to finding a path which is combined of a number of
-   * left and then a number of up movement, or left/down, or right/up, or right/down movements only.
-   * If this is not possible due to walls, it returns an empty string. No actual move is done. If
-   * x or y are outside the field, an empty string is returned as well.
-   */
   def suggestMove(x: Int, y: Int): String = {
     return ""
   }
 
-  
-  /* --- The three save methods below are used by the unit tests to simulate certain conditions --- */
-
-  /**
-   * Updates saveX and saveY to the current player position.
-   */
   def save(): Unit = {
-    /* This method is already implemented. You should not change it */
     saveX = positionX
     saveY = positionY
   }
 
-  /**
-   * Returns the current save position as a tuple, in (x,y) order.
-   */
   def getSavePos(): (Int, Int) = {
-    /* This method is already implemented. You should not change it */
     return (saveX, saveY);
   }
 
-  /**
-   * Sets the savePos to the values of the parameters.
-   */
   def setSavePos(saveX: Int, saveY: Int): Unit = {
-    /* This method is already implemented. You should not change it */
     this.saveX = saveX
     this.saveY = saveY
   }
 
 }
 
-/**
- * This object builds and returns a standard instance of Game.
- * It is used by the unit tests to initialise the game in different states.
- * Currently, there are three ways in which a game can be initialised,
- * the first has been completed but the other two initialisation methods need writing.
- */
 object GameBuilder {
 
-  /**
-   * @return A game with
-   * - walls in positions 3,0 3,1 and 3,2
-   * - a coin at 4,1 which increases score by 100
-   * - a coin at 3,3 which increases score by 250
-   * - the player starting in position 0,0
-   */
   def initialiseGame1(): Game = {
-    /* This method is already implemented. You should not change it */
     return new Game(List((3, 0), (3, 1), (3, 2)), List((4, 1, 100), (3, 3, 250)), 0, 0)
   }
 
-  /**
-   * @return A game with
-   * - walls in positions 3,3 3,4 3,5 5,3 5,4 and 5,5
-   * - a coin at 4,4 which increases score by 200
-   * - a coin at 6,3 which increases score by 200
-   * - the player starting in position 3,2
-   */
   def initialiseGame2(): Game = {
     return new Game(List(), List(), 0, 0)
   }
 
-  /**
-   * @return A game with
-   * - walls in positions 3,0 3,1 and 3,2
-   * - a coin at 4,1 which increases score by 300
-   * - a coin at 3,3 which increases score by 150
-   * - the player starting in position 4,1
-   */
   def initialiseGame3(): Game = {
     return new Game(List(), List(), 0, 0)
   }
